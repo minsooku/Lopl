fc1a1cf4fa30d6c6ad36fce6d291e37ceffd0298
wlgjs678 <wlgjs678@gmail.com>
2023-12-07 14:52:55 -0500

    update

---
+++ lopl.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const ejs = require('ejs');
+const http = require('http');
+const path = require('path');
+require("dotenv").config({ path: path.resolve(__dirname, '.env') })
+
